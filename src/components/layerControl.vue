e340b859d85c9393ece93eb76b2b58d2897596e2
liuhang <1016817543@qq.com>
2018-12-12 08:56:24 +0800

    添加树形控件示例

---
+++ src/components/layerControl.vue
@@ -0,0 +1,44 @@
+<template>
+    <Tree :data="data2" show-checkbox></Tree>
+</template>
+<script>
+export default {
+  name: "LayerControl",
+  data() {
+    return {
+      data2: [
+        {
+          title: "parent 1",
+          expand: true,
+          children: [
+            {
+              title: "parent 1-1",
+              expand: true,
+              children: [
+                {
+                  title: "leaf 1-1-1"
+                },
+                {
+                  title: "leaf 1-1-2"
+                }
+              ]
+            },
+            {
+              title: "parent 1-2",
+              expand: true,
+              children: [
+                {
+                  title: "leaf 1-2-1"
+                },
+                {
+                  title: "leaf 1-2-1"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -17,6 +17,7 @@
                 </Menu>
             </Header>
             <Layout>
+                <LayerControl></LayerControl> 
                 <Sider hide-trigger :style="{background: '#fff'}">
                 </Sider>
                 <Layout>
@@ -30,101 +31,113 @@
     </div>
 </template>
 <script>
-    // import Map from './map.vue'
-    export default {
-        name: "Home",
-        props: {},
-        data() {
-            return {
-                viewer: null
-            };
-        },
-        mounted: function () {
-            this.viewer = new window.Cesium.Viewer("mapContent", {
-                imageryProvider: new window.Cesium.WebMapTileServiceImageryProvider({
-                    url: "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
-                    layer: "tdtBasicLayer",
-                    style: "default",
-                    format: "image/jpeg",
-                    tileMatrixSetID: "GoogleMapsCompatible",
-                    show: false,
-                    maximumLevel: 16
-                }),
-                homeButton: false,
-                navigationHelpButton: false,
-                timeline: false,
-                animation: false,
-                sceneModePicker: false,
-                baseLayerPicker: false,
-                geocoder: false,
-                fullscreenButton: false,
-                navigationInstructionsInitiallyVisible: false
-            });
-            this.viewer._cesiumWidget._creditContainer.style.display = "none";
-        },
-        methods: {
-            changeLayer: function (layerID) {
-                this.viewer.imageryLayers.removeAll();
-                switch (layerID) {
-                    case 'tdt':
-                        this.viewer.imageryLayers.addImageryProvider(new window.Cesium.WebMapTileServiceImageryProvider({
-                            url: "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
-                            layer: "tdtBasicLayer",
-                            style: "default",
-                            format: "image/jpeg",
-                            tileMatrixSetID: "GoogleMapsCompatible",
-                            show: false,
-                            maximumLevel: 16
-                        }));
-                        break;
-                    case 'gg':
-                        let url = 'http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali'
-                        this.viewer.imageryLayers.addImageryProvider(new window.Cesium.UrlTemplateImageryProvider({
-                            url: url
-                        }));
-                        break;
-                    case 'arcgis':
-                        this.viewer.imageryLayers.addImageryProvider(new window.Cesium.ArcGisMapServerImageryProvider({
-                            url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer',
-                            enablePickFeatures: false
-                        }));
-                        break;
-                    default:
-                        break;
-
-                }
-            }
-        }
+import LayerControl from "./layerControl.vue";
+export default {
+  name: "Home",
+  components: {
+    LayerControl
+  },
+  props: {},
+  data() {
+    return {
+      viewer: null
     };
+  },
+  mounted: function() {
+    this.viewer = new window.Cesium.Viewer("mapContent", {
+      imageryProvider: new window.Cesium.WebMapTileServiceImageryProvider({
+        url:
+          "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
+        layer: "tdtBasicLayer",
+        style: "default",
+        format: "image/jpeg",
+        tileMatrixSetID: "GoogleMapsCompatible",
+        show: false,
+        maximumLevel: 16
+      }),
+      homeButton: false,
+      navigationHelpButton: false,
+      timeline: false,
+      animation: false,
+      sceneModePicker: false,
+      baseLayerPicker: false,
+      geocoder: false,
+      fullscreenButton: false,
+      navigationInstructionsInitiallyVisible: false
+    });
+    this.viewer._cesiumWidget._creditContainer.style.display = "none";
+  },
+  methods: {
+    changeLayer: function(layerID) {
+      this.viewer.imageryLayers.removeAll();
+      switch (layerID) {
+        case "tdt":
+          this.viewer.imageryLayers.addImageryProvider(
+            new window.Cesium.WebMapTileServiceImageryProvider({
+              url:
+                "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles",
+              layer: "tdtBasicLayer",
+              style: "default",
+              format: "image/jpeg",
+              tileMatrixSetID: "GoogleMapsCompatible",
+              show: false,
+              maximumLevel: 16
+            })
+          );
+          break;
+        case "gg":
+          let url =
+            "http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali";
+          this.viewer.imageryLayers.addImageryProvider(
+            new window.Cesium.UrlTemplateImageryProvider({
+              url: url
+            })
+          );
+          break;
+        case "arcgis":
+          this.viewer.imageryLayers.addImageryProvider(
+            new window.Cesium.ArcGisMapServerImageryProvider({
+              url:
+                "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+              enablePickFeatures: false
+            })
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+};
 </script>
 <style scoped>
-    .baseLayerChangeClass {
-        width: 50px;
-    }
+.baseLayerChangeClass {
+  width: 50px;
+}
 
-    .layout {
-        border: 1px solid #d7dde4;
-        background: #f5f7f9;
-        position: relative;
-        border-radius: 4px;
-        overflow: hidden;
-        height: 100%;
-    }
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  height: 100%;
+}
 
-    .layout-logo {
-        width: 100px;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        float: left;
-        position: relative;
-        top: 15px;
-        left: 20px;
-    }
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
 
-    .layout-nav {
-        width: 420px;
-        margin: 0 auto;
-        margin-right: 20px;
-    }
+.layout-nav {
+  width: 420px;
+  margin: 0 auto;
+  margin-right: 20px;
+}
 </style>
