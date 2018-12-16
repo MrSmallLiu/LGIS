b95c22a4054ee8c902a62d7ec7dad7bd81bd6760
liuhang <1016817543@qq.com>
2018-12-16 22:05:31 +0800

    修改图层控制树的样式

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
 
 <script>
   import Home from "./components/toolbar.vue";
-
   export default {
     name: "app",
     components: {
-      Home
+      Home,
+      LayerControl
     }
   };
 </script>
@@ -21,7 +21,7 @@
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    /* text-align: center; */
     color: #2c3e50;
     /* margin-top: 60px; */
     height: 100%;
